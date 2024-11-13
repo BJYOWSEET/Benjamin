d99283cc89e23b258534874744bc6d04101f40f4
BJYOWSEET <bjp14941@gmail.com>
2024-11-13 09:50:20 +0800

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,26 @@
+
+package javaapplication3;
+
+
+public class JavaApplication3 {
+
+    
+    public static void main(String[] args) {
+      int iNumber = 3;
+      if (iNumber >= 0)
+          
+      if (iNumber == 0){
+          System.out.println ("first string");
+      }  else {
+      
+       System.out.println ("second string"); 
+     
+      
+ 
+        System.out.println("third string");
+     
+                   
+                   
+  }
+    
+}
